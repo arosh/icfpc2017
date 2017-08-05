eb94aa38b6f03648d012e7eb61311a539d402ee9
Yazaten <Yazaten@google.com>
2017-08-05 22:11:14 +0900

    add compare

--- Makefile
+++ Makefile
@@ -1,3 +1,2 @@
-CXX=g++-7
 CXXFLAGS=-std=gnu++14 -Wall -Wextra -Wshadow -O2 -g
-all: randomAI/randomAI yurahunaAI_1/yurahunaAI_1 yurahunaAI_2/yurahunaAI_2
+all: randomAI/randomAI yurahunaAI_1/yurahunaAI_1 yurahunaAI_2/yurahunaAI_2 calcScore/calcScore
---
+++ communication/compare.py
@@ -0,0 +1,114 @@
+import argparse
+import io
+import json
+from subprocess import PIPE, Popen
+import itertools
+from tqdm import tqdm
+
+from model import *
+from online import getLogger, setup_model_to_stdin, pull_model_to_stdin, parse_output
+
+class Server:
+    def __init__(self, n):
+        filename = 'maps/sample.json'
+        with open(filename) as f:
+            self.graph = json.load(f)
+        self.punters = n
+        self.punter = 0
+        self.lastMove = []
+        for i in range(self.punters):
+            self.lastMove.append(MoveModel(i, -1, -1))
+        self.history = []
+
+    def next(self):
+        self.punter += 1
+        self.punter %= self.punters
+
+    def pushMove(self, move):
+        self.lastMove[self.punter] = move
+        self.history.append(move)
+
+def setup(server):
+    recv = {
+        'punters': server.punters,
+        'punter': server.punter,
+        'map': server.graph,
+    }
+    rivers = []
+    for river in recv['map']['rivers']:
+        rivers.append(RiverModel(
+            source=river['source'], target=river['target']))
+    model = SetupModel(
+        n=recv['punters'],
+        p=recv['punter'],
+        sites=[x['id'] for x in recv['map']['sites']],
+        rivers=rivers,
+        mines=recv['map']['mines'])
+    server.next()
+    return model
+
+def pull(server):
+    moves = []
+    for move in server.lastMove:
+        moves.append(move)
+    model = GameplayModel(moves=moves)
+    return (True, model)
+
+def push(server, move):
+    server.pushMove(move)
+    server.next()
+
+def evaluate(server):
+    o = []
+    setup_stdin = setup_model_to_stdin(setup(server))
+    o.append(setup_stdin)
+    model = GameplayModel(moves=server.history)
+    pull_stdin = pull_model_to_stdin(model)
+    o.append(pull_stdin)
+    output = ''.join(o) + '\n'
+    proc = Popen('./calcScore/calcScore', stdin=PIPE, stdout=PIPE)
+    proc.stdin.write(output.encode('ascii'))
+    proc.stdin.flush()
+    scores = proc.stdout.readline()
+    proc.terminate()
+    scores = list(map(int, scores.split()))
+    return scores
+
+def run(solvers):
+    server = Server(len(solvers))
+    procs = []
+    for i in range(len(solvers)):
+        procs.append(Popen(solvers[i], stdin=PIPE, stdout=PIPE))
+    for proc in procs:
+        setup_model = setup(server)
+        setup_stdin = setup_model_to_stdin(setup_model)
+        proc.stdin.write(setup_stdin.encode('ascii'))
+        proc.stdin.flush()
+
+    for proc in itertools.islice(itertools.cycle(procs), len(setup_model.rivers)):
+        _, pull_model = pull(server)
+        pull_stdin = pull_model_to_stdin(pull_model)
+        proc.stdin.write(pull_stdin.encode('ascii'))
+        proc.stdin.flush()
+        output = proc.stdout.readline()
+        push(server, parse_output(output))
+
+    for proc in procs:
+        proc.stdin.write('-1 -1 -1\n'.encode('ascii'))
+        proc.terminate()
+
+    return evaluate(server)
+
+def main(solvers):
+    winnings = [0] * len(solvers)
+    for i in tqdm(range(40)):
+        scores = run(solvers)
+        winnings[scores.index(max(scores))] += 1
+    for i in range(len(solvers)):
+        print(solvers[i], winnings[i] / 40)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('solvers', nargs='+')
+    args = parser.parse_args()
+    main(args.solvers)
