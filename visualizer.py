253476fe2018c5ea821b546e34c18bde1a187054
Yazaten <Yazaten@google.com>
2017-08-07 17:53:53 +0900

    edit compare and visualizer

--- visualizer.py
+++ visualizer.py
@@ -31,19 +31,25 @@ for (label,x,y) in zip(ids,Xs,Ys):
 	plt.text(x,y,label)
 	plt.scatter(x,y,alpha = 0)
 
+names = []
 count = [0]*16
+first_line = True
 for line in open(log_filename,'r'):
-	l = line.rstrip().split(' ')
-	p,u,v = int(l[0]),int(l[1]),int(l[2])
-	if u==-1 and v==-1 :
-		continue
-	a,b = id2cord[u],id2cord[v]
-
-	if count[p]==0:
-		plt.plot([a[0],b[0]],[a[1],b[1]],color=cm.hsv((p+1)/4),label=str(p))
+	if first_line == True:
+		names = line.rstrip().split(' ')
+		first_line = False
 	else:
-		plt.plot([a[0],b[0]],[a[1],b[1]],color=cm.hsv((p+1)/4))
-	count[p]+=1
+		l = line.rstrip().split(' ')
+		p,u,v = int(l[0]),int(l[1]),int(l[2])
+		if u==-1 and v==-1 :
+			continue
+		a,b = id2cord[u],id2cord[v]
+
+		if count[p]==0:
+			plt.plot([a[0],b[0]],[a[1],b[1]],color=cm.hsv((p+1)/4),label=names[p])
+		else:
+			plt.plot([a[0],b[0]],[a[1],b[1]],color=cm.hsv((p+1)/4))
+		count[p]+=1
 #	plt.text((a[0]+b[0])/2,(a[1]+b[1])/2,count[p],color=cm.hsv((p+1)/10))
 	
 plt.legend()
